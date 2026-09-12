8e8fbcfaab6ebc619d5649e7446624b0a8e2c06f
Amirayg <amir.dota29189@gmail.com>
2026-09-12 11:51:09 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const title = document.querySelector("h1");
-const button = document.querySelector("#helloButton");
-
-button.addEventListener("click", function () {
-    title.classList.toggle("highlight");
-});
 const button = document.querySelector("#helloButton");
 
 button.addEventListener("click", function () {
