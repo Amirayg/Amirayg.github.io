d3b8784f310a58eb82caece71926dd43a8009d8f
Amirayg <amir.dota29189@gmail.com>
2026-09-12 11:53:54 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const button = document.querySelector("#helloButton");
+const addButton = document.querySelector("#addButton");
+const removeButton = document.querySelector("#removeButton");
 
-button.addEventListener("click", function () {
-    const newText = document.createElement("p");
+let newText;
 
+addButton.addEventListener("click", function () {
+    newText = document.createElement("p");
     newText.textContent = "This paragraph was created with JavaScript!";
-
     document.body.appendChild(newText);
+});
+
+removeButton.addEventListener("click", function () {
+    if (newText) {
+        newText.remove();
+    }
 });
