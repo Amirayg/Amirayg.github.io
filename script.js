e0aaae7ef60c6292961430e7f07484a257f7b15b
Amirayg <amir.dota29189@gmail.com>
2026-09-12 11:26:03 +0330

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ console.log("Hello from JavaScript!");
 console.log("AYG");
 console.log(14);
 console.log(20 + 10);
+let hero = "Invoker";
+let games = 200;
+
+const player = "Topson";
+
+console.log(hero);
+console.log(games);
+console.log(player);
+
+games = 250;
+
+console.log(games);
