fda60772707827811544e9065b8367b8ee62ab95
Amirayg <amir.dota29189@gmail.com>
2026-09-12 12:06:37 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const button = document.querySelector("#helloButton");
+const title = document.querySelector("h1");
+
+button.addEventListener("click", function () {
+    title.classList.toggle("highlight");
+});
+
+
 const addButton = document.querySelector("#addButton");
 const removeButton = document.querySelector("#removeButton");
 
@@ -15,6 +23,7 @@ removeButton.addEventListener("click", function () {
     }
 });
 
+
 const nameInput = document.querySelector("#nameInput");
 const showButton = document.querySelector("#showButton");
 const result = document.querySelector("#result");
@@ -22,5 +31,9 @@ const result = document.querySelector("#result");
 showButton.addEventListener("click", function () {
     const name = nameInput.value;
 
-    result.textContent = "Hello " + name + "!";
+    if (name === "") {
+        result.textContent = "Please enter your name!";
+    } else {
+        result.textContent = "Hello " + name + "!";
+    }
 });
