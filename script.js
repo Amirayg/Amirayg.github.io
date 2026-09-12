62a64a55c3c0a64d5493c4ccfa422e01262ed66d
Amirayg <amir.dota29189@gmail.com>
2026-09-12 11:48:24 +0330

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const button = document.querySelector("#helloButton");
 button.addEventListener("click", function () {
     title.classList.toggle("highlight");
 });
+const newText = document.createElement("p");
+
+newText.textContent = "This paragraph was created with JavaScript!";
+
+document.body.appendChild(newText);
