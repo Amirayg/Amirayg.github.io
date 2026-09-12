a3413b6507b8ea90c282c03eb36517aaba5fe7aa
Amirayg <amir.dota29189@gmail.com>
2026-09-12 11:40:00 +0330

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const button = document.querySelector("#helloButton");
 const title = document.querySelector("h1");
 
 button.addEventListener("click", function () {
-    title.textContent = "Welcome to Interactive AYG!";
+    title.style.color = "red";
+    title.style.backgroundColor = "yellow";
 });
